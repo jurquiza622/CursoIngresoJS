ed83dbed430232f4c1d4441e7527639878044182
jurquiza622 <jurquiza622@gmail.com>
2017-02-04 18:35:22 -0300

    ejercicio 8 terminado guia 2

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,21 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var edad;
+    var estadocivil;
+    edad=document.getElementById("edad").value;
+    estadocivil=document.getElementById("estadoCivil").value;
+    if(edad<18 && estadocivil=="Casado")
+    {
+        alert("es muy joven para NO ser soltero");
+
+    }else{
+        if(edad<18 && estadocivil=="Divorciado"){
+            alert("es muy joven para No ser soltero")
+        }
+
+
+    }
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,12 @@
 function Mostrar()
 {
 //tomo la edad  
-
-	
-
+    var edad;
+    var estadoCivil;
+    edad=document.getElementById("edad").value;
+    estadoCivil=document.getElementById("estadoCivil").value;
+    if(edad>=18 && estadoCivil=="Soltero"){
+        alert("es soltero y no es menor")
+    };
 
 }//FIN DE LA FUNCIÓN
