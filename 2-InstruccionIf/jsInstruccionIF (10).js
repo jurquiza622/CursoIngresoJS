b3659e87df65c6cb6d0a04966e83d89860bb9ab8
jurquiza622 <jurquiza622@gmail.com>
2017-02-05 11:38:01 -0300

    ejercicio 9 y 10 guia 2 terminado

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,17 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+	numero=Math.floor(Math.random()*11);
+	if(numero>=9){
+		alert("su nota es: "+numero+ "Excelente");
+	}else{
+		if(numero>=4 && numero<9){
+			alert("su nota es: "+numero+" Aprobo");
+		}else{
+			alert("su nota es: "+numero+" Vamos la proxima se puede");
+		}
+
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,8 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+	numero=Math.floor(Math.random()*11);
+	alert(numero);
 
 }//FIN DE LA FUNCIÓN
